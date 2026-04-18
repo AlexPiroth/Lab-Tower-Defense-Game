f9236804aba7d5e06c2852c019495b41b8865382
AlexPiroth <alexpiroth@gmail.com>
2026-04-18 14:23:10 -0400

    finished basic grid spawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,7 @@ public class GameManager : MonoBehaviour
     static bool continuing = false;
 
     // Grid Dimensions
-    public const int HEIGHT = 9, WIDTH = 15;
-    public int TOP, LEFT;
+    public const int HEIGHT = 9, WIDTH = 16;
 
     // Stats
     int level;
@@ -22,16 +21,20 @@ public class GameManager : MonoBehaviour
     public GameObject[,] grid;
     public GameObject cameraObj;
 
+    public GameObject testTower;
+
     // Wave Loading Checks
     int waveLength;
     bool waveDone = false;
     private readonly WaitForSeconds cooldown = new(1);
+    private readonly WaitForSeconds warmup = new(5);
 
     // Prefabs
     [SerializeField] GameObject[] allEnemyTypes;
-    [SerializeField] GameObject gridPrefab;
+    [SerializeField] GameObject gridPrefab, spawnPointPrefab;
 
     cluster[] clusters;
+    bool startGame = false;
 
     struct cluster
     {
@@ -41,43 +44,82 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (livingEnemies.Length == 0 && waveDone)
+        if (startGame)
         {
-            level++;
-            waveDone = false;
-            SpawnNextWave();
+            if (livingEnemies.Length == 0 && waveDone)
+            {
+                level++;
+                waveDone = false;
+                SpawnNextWave();
+            }
         }
     }
 
     private void Awake()
     {
+        grid = new GameObject[HEIGHT, WIDTH];
         MakeEmptyGrid();
-        grid = new GameObject[25,25];
         if (continuing)
             LoadSave();
+        else
+            MakeInitialGrid();
+
+        // Wait 5 seconds
+        StartCoroutine(nameof(WarmUp));
+
+        // Start the game
+        startGame = true;
     }
 
     private void LoadSave()
     {
-        // Load the saved grid
+        // Load the saved grid, figure this out later
     }
     
     private void MakeEmptyGrid()
     {
+        // Make grid parent object
+        GameObject gridParent = new GameObject();
+        gridParent.name = "grid";
+
+        // Calculate grid dimensions
         Renderer renderer = gridPrefab.GetComponent<Renderer>();
         Camera cameraComp = cameraObj.GetComponent<Camera>();
         Vector2 topLeft = cameraComp.ViewportToWorldPoint(new Vector3(0, 1));
         float size = renderer.bounds.size.x;
+        topLeft.x += size / 2;
+        topLeft.y -= size / 2;
         float xCoord = topLeft.x, yCoord = topLeft.y;
+
+        // Make grid
         for (int i = 0; i < HEIGHT; i++)
         {
             for (int j = 0; j < WIDTH; j++)
             {
-                Instantiate(gridPrefab, new Vector2(xCoord, yCoord), Quaternion.identity);
+                GameObject newCell = Instantiate(gridPrefab, new Vector2(xCoord, yCoord), Quaternion.identity);
+                grid[i,j] = newCell;
+                newCell.transform.SetParent(gridParent.transform);
                 xCoord += size;
             }
             yCoord -= size;
             xCoord = topLeft.x;
+        }
+    }
+
+    private void MakeInitialGrid()
+    {
+        GridSpace gridSpace;
+        for (int i = 0; i < 5; i++)
+        {
+            gridSpace = grid[4, 13 - i].GetComponent<GridSpace>();
+            if (i == 0)
+                gridSpace.SpawnTower(testTower);
+            else if (i == 4)
+            {
+                gridSpace.SpawnTower(spawnPointPrefab);
+            }
+            else
+                gridSpace.SetPath();
         }
     }
 
@@ -118,6 +160,11 @@ public class GameManager : MonoBehaviour
             Instantiate(clusters[j].type, spawnPoint.transform.position, Quaternion.identity); // Spawn an enemy at spawnPoint
 
             yield return cooldown;
+    }
+
+    IEnumerator WarmUp()
+    {
+        yield return warmup;
     }
 
 }
